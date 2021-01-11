9ba785f03b8235ba02342f96790cdf6af7d63a0e
Victor Ryabkov <victor.ryabkov.business@gmail.com>
2021-01-11 16:34:08 +0300

    feature: vertebrae release router

--- src/route/v2/release.ts
+++ src/route/v2/release.ts
@@ -7,42 +7,17 @@ const router = express.Router()
 
 router.get('/', (req: Request, res: Response) => {
   req.protect?.()
-  console.log(db.model('releases'))
-  console.log(db.models)
-  console.log(db.modelManager)
-
-  // Release.findAll()
-  //   .then((response) => console.log(response))
-  //   .catch((err) => console.log(err))
 })
 router.get('/:id', (req: Request, res: Response) => {
   req.protect?.()
 })
 router.post('/', (req: Request, res: Response) => {
   req.protect?.()
-  const data = {
-    channel: { name: 'VDUD' },
-    video: { title: 'test' },
-    guest: { guest: 'Victor Ryabkov' },
-    recommendation: { recommendation: 'test' },
-  }
-  const { channel, video, guest, recommendation } = data
-  Release.create({
-    channel,
-    video,
-    guest,
-    recommendation,
-  })
-    .then((response) => {
-      console.log(response)
-      res.send(response)
-    })
-    .catch((err) => console.log(err))
 })
-router.put('/', (req: Request, res: Response) => {
+router.put('/:id', (req: Request, res: Response) => {
   req.protect?.()
 })
-router.delete('/', (req: Request, res: Response) => {
+router.delete('/:id', (req: Request, res: Response) => {
   req.protect?.()
 })
 
